f739cc7bffb089e9f0ef1ead0cf0b16c22ff98d0
Justin Forcier <zertukis@gmail.com>
2015-02-07 22:34:11 -0600

    added function to convert spaces to underscore for wiki link

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -36,9 +36,15 @@ angular.module('similarMusicApp')
   		success (function(json) {
 				var relatedArtists = json;
 				$scope.relatedArtists = relatedArtists;
-				console.log(relatedArtists);
+        $scope.replaceSpaces($scope.relatedArtists);
+				console.log($scope.relatedArtists);
 		});
-  	};
+    $scope.replaceSpaces = function(relatedArtists) {
+      for (var i = 0; i < relatedArtists.artists.length; i++) {
+        $scope.relatedArtists.artists[i].newName = relatedArtists.artists[i].name.split(' ').join('_');
+      }
+    };
+  };
 
   	// $scope.albumLookUp = function(id) {
   	// 	$http.get('https://api.spotify.com/vi/artists/' + id + '/albums').
