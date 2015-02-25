5376366702e6de0221d50fb0af016beaa47e0f10
Justin Forcier <zertukis@gmail.com>
2015-02-25 02:13:17 -0600

    now also saves top track data for 2nd and 3rd top tracks

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,8 +7,6 @@
  * # MainCtrl
  * Controller of the similarMusicApp
  */
-var test;
-var wikiArray;
 
 angular.module('similarMusicApp')
   .controller('MainCtrl', function ($scope, $http, $sce) {
@@ -39,18 +37,20 @@ angular.module('similarMusicApp')
         $scope.trackLookUp();
 				console.log($scope.relatedArtists);
 		});
+
     $scope.replaceSpaces = function(relatedArtists) {
       for (var i = 0; i < relatedArtists.artists.length; i++) {
         $scope.relatedArtists.artists[i].newName = relatedArtists.artists[i].name.split(' ').join('_');
       }
     };
+
     $scope.trackLookUp = function() {
       for (var i = 0; i < $scope.relatedArtists.artists.length; i++) {
         (function(i) {
           $http.get('https://api.spotify.com/v1/artists/' + $scope.relatedArtists.artists[i].id + '/top-tracks?country=US').
           success (function(json) {
-            $scope.relatedArtists.artists[i].tracks = json.tracks[0];
-            console.log(json.tracks[0]);
+            $scope.relatedArtists.artists[i].tracks = [json.tracks[0], json.tracks[1], json.tracks[2]];
+            console.log($scope.relatedArtists.artists[i].tracks);
             $scope.relatedArtists.artists[i].tracks.song_url = $sce.trustAsResourceUrl($scope.relatedArtists.artists[i].tracks.preview_url);
           }).
           error (function() {
@@ -59,16 +59,16 @@ angular.module('similarMusicApp')
         })(i);
       }
     }
+
     $scope.wikiLookUp = function() {
       for (var i = 0; i < $scope.relatedArtists.artists.length; i++) {
         (function(i) {
           $http.jsonp('http://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exsentences=10&titles=' + $scope.relatedArtists.artists[i].name + '&callback=JSON_CALLBACK').
           success (function(json) {
-            test = json.query.pages;
-              for (var property in test) {
-                  if (test.hasOwnProperty(property)) {
-                    $scope.relatedArtists.artists[i].bio = test[property].extract;
-                    console.log(test[property].extract);
+              for (var property in json.query.pages) {
+                  if (json.query.pages.hasOwnProperty(property)) {
+                    $scope.relatedArtists.artists[i].bio = json.query.pages[property].extract;
+                    console.log(json.query.pages[property].extract);
                     break;
                   }
               }
